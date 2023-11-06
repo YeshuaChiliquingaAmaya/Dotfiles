46426ff7d1df86c46244f730a71741ac2d2ad93f
Yeshua <yeshuachiliquinga@gmail.com>
2023-11-05 23:42:15 -0500

    add firefox and bluetooth config

---
+++ autostart.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#Configurar segundo monitor a la derecha
+mons -e right
+
+#Picon config for all displays
+DISPLAY=":0" picom -b & 
+
+#pulseaudio -k 
+#----Icons system----
+
+# systray battery icon
+#cbatticon -u 5 &
+
+#Bluetooth
+blueberry-tray &
+
+#usb list
+udiskie -t &
+
+#network manager script
+nm-applet &
+
+# systray volume
+# volumeicon &
+
+#------------------
+
+#Wallpaper charge
+nitrogen --restore &
+
+#configuracion teclado espaniol latino
+#setxkbmap la-latin1'
+
+# suspend after 15 minutes of inactivity
+xidlehook --not-when-audio --not-when-fullscreen --timer 900 'systemctl suspend-then-hibernate' '' & # automatically suspend after 15 minutes of inactivity
